13f51e54bc3458257c8eb5f45fff7e04f4dc4596
Cristian Otalora <cristianyesidot@gmail.com>
2026-08-18 14:58:10 -0500

    feat: usage of dummy home

--- home/src/app/app.component.ts
+++ home/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component, VERSION } from "@angular/core";
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
   standalone: true,
+  imports: [RouterOutlet],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = "home";
-  readonly angularVersion = VERSION.full;
-}
+export class AppComponent {}
--- home/src/app/app.routes.ts
+++ home/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
 
-export const routes: Routes = [{ path: "", component: AppComponent }];
+export const routes: Routes = [
+  { path: "", pathMatch: "full", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "**", redirectTo: "" },
+];
